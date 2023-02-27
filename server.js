0c70a80c1b8639d2ea87b388358c2b511f6bb058
최동훈 <choidonghun@choedonghuns-MacBook-Pro.local>
2023-02-27 14:23:44 +0900

    재사용 가능한 부분 컴포넌트화

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const bootstrapcss =
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css";
 const bootstrapjs =
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js";
+
 router.get("/", (req, res) => {
   res.render("home", {
     bootstrapcss,
